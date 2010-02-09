08f4af9a48d4590a131cff73d139bf501a7f3726
Shea Frederick <justarock@gmail.com>
2010-02-09 16:26:54 -0500

    a simple test example

--- src/Ext.ux.Raphael.js
+++ src/Ext.ux.Raphael.js
@@ -31,5 +31,39 @@ Ext.ux.Raphael = Ext.extend(Ext.BoxComponent, {
 
     onResize: function(w, h) {
         this.paper.setSize(w, h);
+    },
+    
+    getPaper: function(){
+    	return this.paper;
+    },
+    
+    createShape: function(shape, x, y, w, h, r, d){
+    
+    	if (Ext.isObject(shape)){
+    		// cmp.createShape({shape:'square', x:290, y:80, w:60, h:40, r:10});
+    		var s = shape;
+    		this['create'+s.shape.substr(0,1).toUpperCase()+s.shape.substr(1,s.shape.length)]({x:s.x, y:s.y, w:s.w, h:s.h, r:s.r, params:s.params});
+    	} else if (Ext.isArray(shape)){
+    		Ext.each(shape, function(s){
+    			this['create'+s.shape.substr(0,1).toUpperCase()+s.shape.substr(1,s.shape.length)]({x:s.x, y:s.y, w:s.w, h:s.h, r:s.r, params:s.params});
+    		}, this);
+    	} else {
+    		// cmp.createShape('square', 300, 90, 70, 50, 10);
+    		this['create'+shape.substr(0,1).toUpperCase()+shape.substr(1,shape.length)]({x:x, y:y, w:w, h:h, r:r, params:d});
+    	}
+    	
+    },
+    
+    createSquare: function(c){
+    	this.getPaper().rect(c.x, c.y, c.w, c.h, c.r);
+    },
+    
+    createEllipse: function(c){
+    	this.getPaper().ellipse(c.x+c.w, c.y+c.w, c.w, c.h);
+    },
+    
+    createCircle: function(c){
+    	this.getPaper().ellipse(c.x+c.r, c.y+c.r, c.r, c.r);
     }
+    
 });
